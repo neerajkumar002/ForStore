86a47f99d0df9dcdae1387d48f922700eb68f588
Neeraj Kumar <neerajkumar.mobail@gmail.com>
2024-11-28 13:26:58 +0530

    Added react router  dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
- 
+import { Outlet } from "react-router-dom";
 
 function App() {
- 
   return (
-   <>
-   </>
-  )
+    <div>
+      {/* navbar */}
+      <div>
+        <Outlet />
+      </div>
+      {/* footer */}
+    </div>
+  );
 }
 
-export default App
+export default App;
